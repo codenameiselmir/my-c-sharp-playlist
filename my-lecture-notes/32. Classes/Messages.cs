c5facb4ec0e3f80fccebc3b6d87675b024233507
Elmir Abaszada <1030510974@erciyes.edu.tr>
2026-04-04 14:59:56 +0300

    "Lesson 32 added"

---
+++ my-lecture-notes/32. Classes/Messages.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _32._Classes
+{
+    internal class Messages
+    {
+
+        /*
+        Messages class
+
+        This class contains several methods
+        that display messages to the console.
+
+        Because this class is static:
+        - we cannot create objects
+        - we call methods directly using the class name
+        */
+
+        public static void Hello()
+        {
+            Console.WriteLine("Hey! Welcome to the program.");
+        }
+
+        public static void Waiting()
+        {
+            Console.WriteLine("I am waiting for something.");
+        }
+
+        public static void Bye()
+        {
+            Console.WriteLine("Bye! Thanks for visiting.");
+        }
+    }
+}
---
+++ my-lecture-notes/32. Classes/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+===========================================================
+Lesson 32 - Classes
+Repository: my-c-sharp-playlist
+File: my-lecture-notes
+===========================================================
+*/
+
+namespace _32._Classes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+            What is a Class?
+
+            A class is a bundle of related code. Classes help organize programs
+            by grouping related methods together.
+
+            In this example we will use a class called "Messages".
+            */
+
+            /*
+            Calling methods from another class.
+
+            Because Messages is a static class,
+            we do not need to create an object.
+            */
+
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to close...");
+            Console.ReadKey();
+        }
+    }
+}
